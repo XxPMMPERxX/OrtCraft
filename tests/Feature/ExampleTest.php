d8153b9b6dcb2b335ad329b9f8d62f2a321f5825
gollilla <soradore2@gmail.com>
2024-07-22 23:29:20 +0900

    update: テストテスト

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,11 +9,11 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
+     * @codingStandardsIgnoreFile
      */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
-        dump($response);
         $response->assertStatus(200);
     }
 }
