123495b5171c6485f7862acfdc3b7acfd880f660
gollilla <soradore2@gmail.com>
2024-07-30 20:52:21 +0900

    add: 色々追加w

---
+++ resources/js/pages/mypage/ServerAuthDialog.vue
@@ -0,0 +1,141 @@
+<template>
+  <Dialog v-model="active">
+    {{ input.platform }}
+    <h3 class="text-lg font-bold text-center my-2">サーバ認証</h3>
+
+    <ul class="steps w-full">
+      <li class="step step-success">サーバ登録</li>
+      <li class="step step-success">サーバ認証</li>
+      <li class="step">完了</li>
+    </ul>
+
+    <div>
+      <div class="flex my-5 gap-3 md:gap-10 justify-center">
+        <label class="flex gap-1 md:gap-2 items-center cursor-pointer">
+          <input
+            v-model="input.platform"
+            :value="SERVER_PLATFORM_TYPE.JAVA.value"
+            type="radio"
+            name="platform"
+            class="radio radio-xs"
+          />
+          <span>
+            {{ SERVER_PLATFORM_TYPE.JAVA.label }}
+          </span>
+        </label>
+        <label class="flex gap-1 md:gap-2 items-center cursor-pointer">
+          <input
+            v-model="input.platform"
+            :value="SERVER_PLATFORM_TYPE.BE.value"
+            type="radio"
+            name="platform"
+            class="radio radio-xs"
+          />
+          <span>
+            {{ SERVER_PLATFORM_TYPE.BE.label }}
+          </span>
+        </label>
+        <label class="flex gap-1 md:gap-2 items-center cursor-pointer">
+          <input
+            v-model="input.platform"
+            :value="SERVER_PLATFORM_TYPE.JAVA_BE.value"
+            type="radio"
+            name="platform"
+            class="radio radio-xs"
+          />
+          <span>
+            {{ SERVER_PLATFORM_TYPE.JAVA_BE.label }}
+          </span>
+        </label>
+      </div>
+
+      <div class="flex flex-col gap-2">
+        <input
+          v-model="input.name"
+          class="input input-bordered w-full"
+          placeholder="サーバー名 (例. 元気ニコニコ鯖)"
+        />
+        <input
+          v-model="input.address"
+          class="input input-bordered w-full"
+          placeholder="IP (例. locahost)"
+        />
+        <input
+          v-model="input.je_port"
+          class="input input-bordered w-full"
+          placeholder="Java版 PORT (例. 65535)"
+        />
+        <input
+          v-model="input.be_port"
+          class="input input-bordered w-full"
+          placeholder="統合版 PORT (例. 19132)"
+        />
+      </div>
+
+      <button
+        @click="register()"
+        class="btn btn-accent text-white w-full mt-5"
+        :disabled="loading"
+      >
+        <span
+          class="loading loading-spinner"
+          v-if="loading"
+        ></span>
+        登録
+      </button>
+    </div>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios from '@/axios';
+import Dialog from '@/components/dialog/Dialog.vue';
+import { SERVER_PLATFORM_TYPE } from '@/enums';
+import { pushAlert } from '@/composables/alert';
+
+const props = defineProps<{
+  serverData: object,
+}>();
+
+const active = defineModel({
+  default: false
+});
+
+const inputDefault = {
+
+};
+/**
+ * 登録内容
+ */
+const input = ref(inputDefault);
+
+const loading = ref(false);
+
+const register = () => {
+  loading.value = true;
+  axios.post('/api/server', input.value).then((response) => {
+    pushAlert({
+      message: 'サーバの登録を行いました。続けて認証を行なってください。',
+      color: 'success',
+      close_at: 5,
+    });
+    // 登録されたサーバのデータを親コンポーネントに渡す
+    emit('registerd:server', response.data.data);
+  }).catch(() => {
+    pushAlert({
+      message: 'サーバの登録に失敗しました。時間をおいて再度お試しください。',
+      color: 'error',
+      closeable: true,
+    });
+  }).finally(() => {
+    /**
+     * ロードをストップして、ダイアログを閉じる
+     * 入力をリセット
+     */
+    loading.value = false;
+    active.value = false;
+    input.value = inputDefault;
+  });
+};
+</script>
